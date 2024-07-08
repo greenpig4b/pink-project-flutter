c7daae975e13abd4c13952b5abc89f8ebcb7abbc
kimjihun <greenpig4b@naver.com>
2024-07-08 12:31:09 +0900

    기초세팅

---
+++ lib/ui/main/chart/chart_page.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ChartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('차트페이지'),
+      ),
+    );
+  }
+}
---
+++ lib/ui/startview/startview_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StartviewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('로그인페이지'),
+      ),
+    );
+  }
+}
