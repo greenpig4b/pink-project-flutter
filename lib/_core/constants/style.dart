df12fb718c087d160232c62b1e73909f5b129a29
kimjihun <greenpig4b@naver.com>
2024-07-08 09:00:41 +0900

    기본세팅

---
+++ lib/_core/constants/style.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+TextStyle h4({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 34, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle h5({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle h6({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle h7({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle h8({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle subtitle1({Color mColor = Colors.black, FontWeight? fontWeight}) {
+  return TextStyle(fontSize: 16, color: mColor, fontWeight: fontWeight);
+}
+
+TextStyle subtitle2({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 14, color: mColor);
+}
+
+TextStyle subtitle3({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 10, color: mColor);
+}
+
+TextStyle overLine({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: mColor);
+}
+
+TextStyle body1({Color mColor = Colors.black}) {
+  return TextStyle(fontSize: 16, color: mColor);
+}
