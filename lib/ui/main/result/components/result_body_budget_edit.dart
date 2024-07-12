400620c3ba7de9d95205613844a4965c278aacd3
lhyeonnit <lhyeonnit@gmail.com>
2024-07-12 16:43:28 +0900

    result/budget/edit/info

--- lib/ui/main/result/components/result_body_budget_edit.dart
+++ lib/ui/main/result/components/result_body_budget_edit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pinkpig_project_flutter/ui/main/result/components/result_body_budget_app_bar.dart';
+import 'package:pinkpig_project_flutter/ui/main/result/components/result_body_budget_edit_info.dart';
 
 class ResultBodyBudgetEdit extends StatelessWidget {
   const ResultBodyBudgetEdit({Key? key});
@@ -7,8 +8,17 @@ class ResultBodyBudgetEdit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: ResultBodyBudgetAppBar(title: "예산"),
-      body: Placeholder(),
+      body: Column(
+        children: [
+          ResultBodyBudgetEditInfo(),
+          Container(
+            height: 0.5, // 선의 높이 설정 (굵기)
+            color: Colors.grey, // 선의 색상 설정
+          ),
+        ],
+      ),
     );
   }
 }
