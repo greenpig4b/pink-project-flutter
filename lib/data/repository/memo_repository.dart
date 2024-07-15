298d085dafe1e58bc1567d751b7d6d38033a9687
lhyeonnit <lhyeonnit@gmail.com>
2024-07-15 18:26:50 +0900

    memo response save, edit, delete 생성

--- lib/data/repository/memo_repository.dart
+++ lib/data/repository/memo_repository.dart
@@ -13,7 +13,23 @@ class MemoRepository {
 
     return responseDTO;
   }
+  Future<ResponseDTO> editMemo(int memoId, MemoUpdateDTO requestDTO) async {
+    final response = await dio.put("/api/memos/$memoId", data: requestDTO.toJson());
+    ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
 
+    return responseDTO;
+  }
+  Future<ResponseDTO> deleteMemo(int memoId) async {
+    try {
+      final response = await dio.delete("/api/memos/$memoId");
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      return responseDTO;
+    } catch (e) {
+      // 예외 처리
+      print("메모 삭제 중 오류 발생: $e");
+      rethrow; // 예외를 다시 던져서 상위 코드에서 처리할 수 있도록 함
+    }
+  }
   Future<ResponseDTO> fetchMemoList(int year, int month,String accessToken) async {
     final response = await dio.get("/api/memos/monthly",
         options: Options(headers: {"Authorization": "${accessToken}"}),
