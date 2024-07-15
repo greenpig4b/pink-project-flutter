6b1617174bdaac57d43ec142c0e9be33f7f43106
lhyeonnit <lhyeonnit@gmail.com>
2024-07-15 16:23:21 +0900

    memo/list/riverpod 성공

--- lib/ui/main/transaction/memo/components/memo_list.dart
+++ lib/ui/main/transaction/memo/components/memo_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart'; // intl 패키지 추가
+import 'package:intl/intl.dart';
 import 'package:pinkpig_project_flutter/ui/main/transaction/memo/components/memo_edit.dart';
 import 'memo_provider.dart';
 
@@ -12,71 +12,82 @@ class MemoList extends ConsumerWidget {
     final memoList = ref.watch(memoListProvider);
 
     if (memoList.isEmpty) {
-      return Center(child: CircularProgressIndicator());
+      return Center(
+        child: Text(
+          '메모를 작성 해주세요.',
+          style: TextStyle(fontSize: 18, color: Colors.black45),
+        ),
+      );
     }
 
-    return ListView.builder(
-      itemCount: memoList.length,
-      itemBuilder: (context, index) {
-        final memo = memoList[index];
-        String formattedDate = DateFormat('MM.dd').format(memo.createdDate);  // MM.dd 형식으로 날짜 형식화
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: memoList.length,
+            itemBuilder: (context, index) {
+              final memo = memoList[index];
+              String formattedDate = DateFormat('MM.dd').format(memo.createdDate); // MM.dd 형식으로 날짜 형식화
 
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Card(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    formattedDate,  // 형식화된 날짜 사용
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  GestureDetector(
-                    onTap: () {
-                      ref.read(titleProvider.notifier).state = memo.title;
-                      ref.read(contentProvider.notifier).state = memo.content;
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MemoEdit(
-                            memo: memo,
-                            date: formattedDate,  // 형식화된 날짜 사용
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          formattedDate, // 형식화된 날짜 사용
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            memo.title,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
+                        SizedBox(height: 8),
+                        GestureDetector(
+                          onTap: () {
+                            ref.read(titleProvider.notifier).state = memo.title;
+                            ref.read(contentProvider.notifier).state = memo.content;
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MemoEdit(
+                                  memo: memo,
+                                  date: formattedDate, // 형식화된 날짜 사용
+                                ),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 10.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  memo.title,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 4),
+                                Text(memo.content),
+                                SizedBox(height: 4),
+                              ],
                             ),
                           ),
-                          SizedBox(height: 4),
-                          Text(memo.content),
-                          SizedBox(height: 4),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
-        );
-      },
+        ),
+      ],
     );
   }
 }
--- lib/ui/main/transaction/memo/components/memo_provider.dart
+++ lib/ui/main/transaction/memo/components/memo_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../_components/calender_widget.dart';
 import '../data/memo_dummy.dart';
 
 // 제목 상태 관리
@@ -7,7 +8,6 @@ final titleProvider = StateProvider<String>((ref) => '');
 // 내용 상태 관리
 final contentProvider = StateProvider<String>((ref) => '');
 
-// StateNotifier class to manage date state
 class SelectedDateNotifier extends StateNotifier<DateTime> {
   SelectedDateNotifier() : super(DateTime.now());
 
@@ -19,16 +19,10 @@ class SelectedDateNotifier extends StateNotifier<DateTime> {
 // Providers
 final selectedDateProvider = StateNotifierProvider<SelectedDateNotifier, DateTime>((ref) => SelectedDateNotifier());
 
-final selectedMonthProvider = StateProvider<DateTime>((ref) {
-  return DateTime.now();
-});
 
 final memoListProvider = Provider<List<Memo>>((ref) {
-  final selectedMonth = ref.watch(selectedMonthProvider);
+  final selectedMonth = ref.watch(calendarProvider);
   return newMemoList.where((memo) {
     return memo.createdDate.year == selectedMonth.year && memo.createdDate.month == selectedMonth.month;
   }).toList();
 });
-
-
-
