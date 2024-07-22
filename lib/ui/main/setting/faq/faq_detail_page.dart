3065bcce8618daf18ea43ffbc229b779de9acd20
coderyu5523@naver.com <coderyu5523@naver.com>
2024-07-22 10:53:20 +0900

    [fix] setting list

---
+++ lib/ui/main/setting/faq/faq_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FaqDetailPage extends StatelessWidget {
+  const FaqDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
